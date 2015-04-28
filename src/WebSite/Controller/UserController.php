e75b00c8a66fd78d15e5e5e5c07c3c6ecfe43899
HugoLambrinidis <hlambrinidis@gmail.com>
2015-04-28 11:29:34 +0200

    first commit
    First commit

---
+++ src/WebSite/Controller/UserController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Website\Controller;
+
+use Doctrine\DBAL;
+
+class UserController{
+    public function listUserAction($request){
+        $config = new \Doctrine\DBAL\Configuration();
+        $connectionParams = array(
+            'dbname' => 'mydb',
+            'user' => 'user',
+            'password' => 'secret',
+            'host' => 'localhost',
+            'driver' => 'pdo_mysql'
+        );
+
+        $conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
+        $statement = $conn->prepare('SELECT * from user');
+        $statement->execute();
+        $users = $statement->fetchAll();
+
+        return [
+            'view' => 'WebSite/View/user/listUser.html.php',
+            'users'=> $users
+        ];
+    }
+    public function addUser($request){
+        if($request['request']){
+            return [
+                'redirect_to' => 'http://.......'
+            ];
+        }
+        return [
+            'view' => 'WebSite/View/user/listUser.html.php',
+            'users'=> $users
+        ];
+    }
+    public function deleteUser($request){
+        return [
+            'redirect_to' => 'http://.......'
+        ];
+    }
+    public function logUser($request){
+        if($request['request']){
+
+        }
+        return [
+            'redirect_to' => 'http://.......'
+        ];
+    }
+}
---
+++ web/index.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This is you FrontController, the only point of access to your webapp
+ */
+/**
+ * Use Yaml components for load a config routing, $routes is in yaml app/config/routing.yml :
+ *
+ * Url will be /index.php?p=route_name
+ *
+ *
+ */
+require_once __DIR__.'/../vendor/autoload.php';
+use Symfony\Component\Yaml\Parser;
+$yaml = new Parser();
+$routes = $yaml->parse(file_get_contents('../app/config/routing.yml'));
+if(isset($_GET['p'])) {
+    $currentRoute = $routes[$_GET['p']]['controller'];
+    $routesArray = explode(':', $currentRoute);
+    var_dump($routesArray);
+//ControllerClassName, end name is ...Controller
+    $controller_class = $routesArray[0];
+//ActionName, end name is ...Action
+    $action_name = $routesArray[1];
+    $controller = new $controller_class();
+//$Request can by an object
+    $request['request'] = &$_POST;
+    $request['query'] = &$_GET;
+//...
+//$response can be an object
+    $response = $controller->$action_name($request);
+    /**
+     * Use Twig !
+     */
+    require $response['view'];
+}
