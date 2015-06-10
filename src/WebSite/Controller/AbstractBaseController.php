9ec8d74eca5fe1816866123041777e107af8935d
HugoLambrinidis <hlambrinidis@gmail.com>
2015-06-10 13:22:51 +0200

    Update Abstract
    add db connections for user by type

--- src/WebSite/Controller/AbstractBaseController.php
+++ src/WebSite/Controller/AbstractBaseController.php
@@ -1,7 +1,32 @@
 <?php
 namespace Website\Controller;
+
+
 use Symfony\Component\Yaml\Parser;
+use WebSite\Model\UserManager;
+use WebSite\Model\ProfsManager;
+use Website\Model\RootManager;
+use WebSite\Model\ProductManager;
+
+
 abstract class AbstractBaseController {
+
+    protected function getUserManager(){
+        return new UserManager($this->getConnection());
+    }
+
+    protected function getProfManager(){
+        return new ProfsManager($this->getConnection());
+    }
+
+    protected function getRootManager(){
+        return new RootManager($this->getConnection());
+    }
+
+    protected function getProductManager(){
+        return new ProductManager($this->getConnection());
+    }
+
     public function getConnection() {
         $config = new \Doctrine\DBAL\Configuration();
         $yaml = new Parser();
